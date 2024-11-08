b4232a02717be9cf530d5ddcbcb7d34abd3b3fb1
Mattia-Carbonera <mattia.carbonera@gmail.com>
2024-11-08 13:28:13 +0100

    add layover

--- script.js
+++ script.js
@@ -2,39 +2,55 @@
 const cardContainer = document.getElementById("card-container");
 
 // genero le card
-fetch("https://jsonplaceholder.typicode.com/photos?_limit=6").then((Response) =>
-  Response.json().then((data) => {
-    // codice
-    // console.log(data[0].url)
-    const objectsArray = data;
 
-    for (let i = 0; i < 6; i++) {
-      const currentPhoto = objectsArray[i].url;
-      const currentTitle = objectsArray[i].title;
+const fetchCard = async () => {
+  const response = await fetch(
+    "https://jsonplaceholder.typicode.com/photos?_limit=6"
+  ).then((Response) =>
+    Response.json().then((data) => {
+      // codice
+      const objectsArray = data;
 
-      // trasformo le prime lettere in maiuscolo
-      const currentTitleSplitted = currentTitle.split(" ");
-      let upperCaseTitol = "";
+      for (let i = 0; i < 6; i++) {
+        const currentPhoto = objectsArray[i].url;
+        const currentTitle = objectsArray[i].title;
 
-      currentTitleSplitted.map(
-        (word) =>
-          (upperCaseTitol += word.charAt(0).toUpperCase() + word.slice(1) + " ")
-      );
+        // trasformo le prime lettere in maiuscolo
+        const currentTitleSplitted = currentTitle.split(" ");
+        let upperCaseTitol = "";
 
-      //   stampo in HTML
-      cardContainer.innerHTML += `<div class="card col-md-5 col-lg-3">
-            <img src="${currentPhoto}" alt="Photo" />
-            <p>${upperCaseTitol}</p>
-            <div class="circle">
-              <img src="./img/pin.svg" alt="" />
-            </div>
-          </div>`;
-    }
-  })
-);
+        currentTitleSplitted.map(
+          (word) =>
+            (upperCaseTitol +=
+              word.charAt(0).toUpperCase() + word.slice(1) + " ")
+        );
 
-// const stringaProva = "ciao oggi vado in palestra".split(" ");
-// const newString = stringaProva.map(
-//   (word) => word.charAt(0).toUpperCase() + word.slice(1)
-// );
-// console.log(newString);
+        //   stampo in HTML
+        cardContainer.innerHTML += `<div class="card col-md-5 col-lg-3" id="card-id">
+                <img src="${currentPhoto}" alt="Photo" />
+                <p>${upperCaseTitol}</p>
+                <div class="circle">
+                  <img src="./img/pin.svg" alt="" />
+                </div>
+              </div>`;
+      }
+
+      // gestisco il layover
+      const layoverContainer = document.getElementById("layover");
+      const CardEl = document.querySelectorAll(".card");
+      const ButtonLayover = document.getElementById("layover-button-close");
+
+      CardEl.forEach((element) => {
+        element.addEventListener("click", function (event) {
+          layoverContainer.classList.remove("d-none");
+        });
+      });
+
+      ButtonLayover.addEventListener("click", function () {
+        layoverContainer.classList.add("d-none");
+      });
+    })
+  );
+};
+
+fetchCard();
